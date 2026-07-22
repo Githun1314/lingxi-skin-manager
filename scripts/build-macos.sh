ca20be22f6753770a41463ad114657fadbf5f78e
Luos <89338119+Githun1314@users.noreply.github.com>
2026-07-23 00:28:31 +0800

    remove stacked corner layer (#3)

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -7,7 +7,7 @@ APP_NAME="WPS灵犀皮肤管理器.app"
 APP_PATH="$WORKSPACE_DIR/$APP_NAME"
 RESOURCE_DIR="$APP_PATH/Contents/Resources/manager"
 DIST_DIR="$WORKSPACE_DIR/dist"
-ZIP_PATH="$DIST_DIR/WPS灵犀皮肤管理器-v0.6.3-macOS.zip"
+ZIP_PATH="$DIST_DIR/WPS灵犀皮肤管理器-v0.6.4-macOS.zip"
 
 mkdir -p "$RESOURCE_DIR" "$DIST_DIR"
 rsync -a --delete --delete-excluded --exclude '.DS_Store' --exclude 'scripts' --exclude 'tmp' "$SOURCE_DIR/" "$RESOURCE_DIR/"
